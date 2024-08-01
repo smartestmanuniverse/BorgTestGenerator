5b4be6c47f9cfd0e12584b18efb71739d0d7c09a
0x07cb <83157348+0x07CB@users.noreply.github.com>
2024-08-01 19:48:03 +0200

    Refactor AgentChefDeDocumentation class for better organization and maintainability

--- BorgTestGenerator/agents/swarm/docs_swarm/agent_chef_de_documentation.py
+++ BorgTestGenerator/agents/swarm/docs_swarm/agent_chef_de_documentation.py
@@ -2,13 +2,149 @@
 
 from typing import Any
 
-class AgentChefDeDocumentation:
-    def __init__(self):
+"""
+Importe ici les modules nécessaires. (externes)
+"""
+# on a besoin de quoi pour completer ce code source ?
+from typing import Optional, Any, Union
+import pkg_resources
+import uuid
+
+
+"""
+Si besoin:
+Importe ici les modules nécessaires. (interne a BorgTestGenerator)
+"""
+# on a besoin de quoi pour completer ce code source ?
+from ...base_agent import Agent
+from ....utils.files import read_text_file
+
+class FilesListToUpload(list):
+    def __init__(self, 
+                 files: Optional[list[str]] = [],
+                 vector_store_name: Optional[str] = None):
+        """
+        Initializes a BaseAgentWriter object.
+
+        Args:
+            files (list[str], optional): A list of file paths. Defaults to an empty list.
+            vector_store_name (str|None, optional): The name of the vector store. Defaults to None.
+        """
+        super().__init__(files)
+        self.vector_store_name = vector_store_name
+    
+    def is_empty(self) -> bool:
+        """
+        Checks if the agent is empty.
+
+        Returns:
+            bool: True if the agent is empty, False otherwise.
+        """
+        return len(self) == 0
+
+    def is_unnamed(self) -> bool:
+        """
+        Checks if the agent is unnamed.
+
+        Returns:
+            bool: True if the agent is unnamed, False otherwise.
+        """
+        return (type(self.vector_store_name) == None) or (len(f"{self.vector_store_name}") == 0)
+
+    def add_file(self, filepath: str) -> None:
+        """
+        Adds a file to the agent's list of files.
+
+        Args:
+            filepath (str): The path of the file to be added.
+
+        Returns:
+            None
+        """
+        self.append(filepath)
+
+    def add_files(self, files: list[str]) -> None:
+        """
+        Adds a list of files to the agent.
+
+        Args:
+            files (list[str]): A list of file paths to be added.
+
+        Returns:
+            None
+        """
+        for file in files:
+            self.add_file(file)
+
+    def remove_file(self, filepath: str) -> None:
+        """
+        Removes a file from the specified filepath.
+
+        Args:
+            filepath (str): The path of the file to be removed.
+
+        Returns:
+            None
+        """
+        self.remove(filepath)
+
+    def list_files(self) -> list[str]:
+        """
+        Returns a list of files.
+
+        Returns:
+            A list of file names.
+        """
+        return self
+    
+    def set_vector_store_name(self, vector_store_name: str) -> None:
+        """
+        Sets the name of the vector store.
+
+        Args:
+            vector_store_name (str): The name of the vector store.
+
+        Returns:
+            None
+        """
+        self.vector_store_name = vector_store_name
+        
+    def get_vector_store_name(self) -> str:
+        """
+        Returns the name of the vector store.
+
+        Returns:
+            str: The name of the vector store.
+        """
+        return self.vector_store_name
+    
+
+
+class AgentChefDeDocumentation(Agent):
+    def __init__(self, assistant_id: Optional[str] = None):
+        super().__init__(assistant_id)
         """
         Initialise un nouvel Agent Chef de Documentation.
         """
         self.nom: str = "Agent Chef de Documentation"
         self.role: str = "Gérer, organiser et diffuser les documents et informations de manière efficace et conforme aux régulations."
+        self.FilesToUpload = FilesListToUpload()
+        self.define_agent_presets()
+
+    def define_agent_presets(self) -> object:
+        """
+        Defines the presets for the UnitTestWriter agent.
+
+        Returns:
+            object: The UnitTestWriter object with the defined presets.
+        """
+        self.agent_name = "UnitTestWriter"
+        self.agent_model = "gpt-4o"
+        self.agent_act_as = read_text_file(pkg_resources.resource_filename(__name__, "prompts/docs_swarm/Agent_Chef_de_Documentation_Prompt.txt"))
+        self.agent_instructions = ""
+        self.tool_code_interpreter = True
+        self.tool_file_search = True
+        return self
 
     def organiser_documents(self) -> None:
         """
