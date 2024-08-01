206eff6a21feacbf9a6b2fa9738ce4609a01c56b
0x07cb <83157348+0x07CB@users.noreply.github.com>
2024-08-01 10:51:37 +0200

    Refactor test_files.py for improved readability and maintainability

--- tests/test_files.py
+++ tests/test_files.py
@@ -3,14 +3,20 @@ from BorgTestGenerator.utils.files import read_text_file
 
 class TestFiles(unittest.TestCase):
     def test_read_text_file(self):
+        from os import urandom
+        with open("/tmp/case_1.txt", "w") as f:
+            f.write("This is the content of the file.")
+            f.close()
         # Test case 1: Existing file
-        file_path = "/path/to/existing/file.txt"
+        file_path = "/tmp/case_1.txt"
         expected_output = "This is the content of the file."
         self.assertEqual(read_text_file(file_path), expected_output)
 
-        # Test case 2: Non-existing file
-        file_path = "/path/to/non_existing/file.txt"
-        self.assertIsNone(read_text_file(file_path))
+        # Test case 2: Non-existing file ( exception : FileNotFoundError )
+        file_path = "/tmp/case_2.txt"
+        with self.assertRaises(FileNotFoundError):
+            read_text_file(file_path)
+        
 
 if __name__ == "__main__":
     unittest.main()
