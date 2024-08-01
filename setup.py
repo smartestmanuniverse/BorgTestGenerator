e34e9a3ab5a4848bb800522351caab29bad5ac8a
0x07cb <83157348+0x07CB@users.noreply.github.com>
2024-08-01 15:46:41 +0200

    Refactor setup.py to update version to 0.3.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='BorgTestGenerator',
-    version='0.3.1',
+    version='0.3.7',
     packages=find_packages(),
     install_requires=[
         'openai', 
