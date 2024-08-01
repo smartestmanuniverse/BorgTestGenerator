1b585522a3ffc4e5c775fd6884128ecc2daa0510
0x07cb <83157348+0x07CB@users.noreply.github.com>
2024-08-01 16:14:40 +0200

    chore: Bump version to 0.3.8 in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='BorgTestGenerator',
-    version='0.3.7',
+    version='0.3.8',
     packages=find_packages(),
     install_requires=[
         'openai', 
